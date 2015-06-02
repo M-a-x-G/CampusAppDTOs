3c3d9f8f56176984f5032233bf5d676d25f798fc
Basti <muellers@fh-brandenburg.de>
2015-06-02 12:44:40 +0200

    * added new DTO class

---
+++ src/main/java/de/fhb/ca/dto/SuccessDTO.java
@@ -0,0 +1,15 @@
+package de.fhb.ca.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Admin on 02.06.2015.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SuccessDTO {
+    private String successMessage;
+}
