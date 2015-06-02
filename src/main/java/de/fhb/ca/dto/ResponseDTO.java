5906b24c1d1270294f341a73eb35a323abf6e09a
Max Gregor <Gregor@fh-brandenburg.de>
2015-06-02 19:56:55 +0200

    add responseDTO

--- src/main/java/de/fhb/ca/dto/ResponseDTO.java
+++ src/main/java/de/fhb/ca/dto/ResponseDTO.java
@@ -4,12 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Created by Admin on 02.06.2015.
- */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class SuccessDTO {
-    private String successMessage;
+public class ResponseDTO {
+    private String message;
 }
