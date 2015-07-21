189c34877af2cc5313464d0dc1cbdc7180578797
Max Gregor <Gregor@fh-brandenburg.de>
2015-07-21 17:30:34 +0200

    changed error type

---
+++ src/main/java/de/fhb/ca/dto/util/ErrorType.java
@@ -0,0 +1,10 @@
+package de.fhb.ca.dto.util;
+
+public class ErrorType {
+    public static final int UNKNOWN_ERROR = -1;
+    public static final int INVALID_TOKEN = 1;
+    public static final int ANSWERS_SUCCESSFULLY_ADDED = 2;
+    public static final int TOKEN_ALLREADY_USED = 3;
+    public static final int EVALUATION_CLOSED = 4;
+    public static final int MALFORMED_REQUEST = 5;
+}
